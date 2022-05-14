26c5c93c5a140fe3691a7faefc27190e0f74ea74
mahaubin <mahaubin@gmail.com>
2022-05-14 14:47:17 +0300

    connexion à la babse de données local

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const mongoose = require('mongoose');
 
+const userRoutes = require('./routes/user');
 const app = express();
+
+// recuperer les contenus de notre body
+app.use(express.json());
+
+//connection à la base de données
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connecté à Mongoose")
+});
+
 
 module.exports= app;
